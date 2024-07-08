fab35034f484c7a1e0688e12788c481a7e481478
song <usnara9465@naver.com>
2024-07-08 17:36:38 +0900

    chap5

---
+++ section05/src/App.jsx
@@ -0,0 +1,48 @@
+import './App.css'
+import Header from "./components/Header.jsx" //파일 확장자 생략 가능
+import Main from "./components/Main.jsx"
+import Footer from "./components/Footer.jsx"
+import Button from "./components/Button.jsx"
+import {useState} from "react";
+
+/* 
+function App() { //app(이름) component //부모 컴포넌트(렌더링 되려면 app컴포넌트에 있어야됨)
+
+const buttonProps = { //spread 연산자 : ...사용
+    text : "메일",
+    color : "red",
+    a : 1,
+    b : 2,
+    c : 3,
+  }
+  return (
+    <>
+    test
+    <Header />
+    <Main />
+    <Footer />
+    <Button {...buttonProps} /> 
+    <Button text={"카페"}/>
+    <Button text={"블로그"}>
+      <div>자식요소</div> */{/* props : childeren으로 넘어감 */}
+    {/*</Button> */}            {/*반대로 자식->부모는 불가 */}
+ //   </>
+//  )
+//}
+
+function App() {
+  const [state, setState] = useState();
+  console.log(state);
+  console.log(setState);
+
+  return <>
+    <h1>{state}</h1>
+    <button onClick = {() => {
+    
+  }}
+      
+    </button>
+  </>;
+}
+
+export default App
---
+++ section05/src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+// const Hello = () => {
+//   return <div>Hello</div>
+// }
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App />
+    //</Hello>
+);
