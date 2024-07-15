1fa097b098702c09fed1a443f57eb62fe2df3948
usongee <154314283+sajdflksjdlfk@users.noreply.github.com>
2024-07-15 12:33:02 +0900

    .

--- section09/src/App.jsx
+++ section09/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import Editor from './components/Editor';
 import List from './components/List';
+import Exam from './components/Exam';
 import './App.css'
 import { useState, useRef } from 'react';
 
@@ -64,9 +65,10 @@ function App() {
   return (
 
     <div className="App">
-      <Header/>
+      <Exam/>
+      {/* <Header/>
       <Editor onCreate = {onCreate}/>
-      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete}/>
+      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete}/> */}
     </div>
   )
 }
---
+++ section09/src/components/Exam.jsx
@@ -0,0 +1,14 @@
+import { useReducer } from "react";
+
+
+const Exam = () => {
+const [state, dispatch] = useReducer();
+
+    return (
+    <div>
+        <h1>0</h1>
+        <button>+</button>
+    </div>
+    )
+}
+export default Exam;
