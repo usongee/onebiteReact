333b32e3d060a393e2bb41e9ca71458d5b166f27
song <usnara9465@naver.com>
2024-07-15 14:17:12 +0900

    s

--- section09/src/components/Exam.jsx
+++ section09/src/components/Exam.jsx
@@ -1,13 +1,28 @@
 import { useReducer } from "react";
 
+function reducer(state, action) {
+    console.log(state, action);
+    if(action.type==='INCREASE') {
+        return state + action.data;
+    }
+}
+
 
 const Exam = () => {
-const [state, dispatch] = useReducer();
+    const [state, dispatch] = useReducer(reducer, 0);
+
+    const onClickPlus = () => {
+        //액션객체
+        dispatch({
+            type: "INCREASE",
+            data : 1,
+        })
+    }
 
     return (
     <div>
-        <h1>0</h1>
-        <button>+</button>
+        <h1>{state}</h1>
+        <button onClick={onClickPlus}>+</button>
     </div>
     )
 }
